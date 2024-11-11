795e81faf6732b8428ff0d636420e995bc96e043
Yogeeswaran03 <yogees97@gmail.com>
2024-11-11 21:11:15 +0530

    changess

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -20,14 +20,14 @@ const Signin = () => {
   };
 
   return (
-    <div className="flex flex-col md:flex-row min-h-screen items-center justify-center p-4 md:p-16 md:px-20">
+    <div className="flex flex-col md:flex-row h-screen items-center justify-center p-4 md:p-16 md:px-20 bg-white">
       {/* Parent container with flex and equal height */}
       <div className="flex flex-col-reverse md:flex-row w-full h-full md:w-3/4 max-w-screen-xl">
         
         {/* Left Section - Content */}
-        <div className="w-full md:w-1/2 flex flex-col justify-center items-center p-8 md:p-12 rounded-xl border border-gray-300">
-          <h2 className="text-3xl font-bold mb-4">Welcome back!</h2>
-          <div className="text-center mb-8 text-sm">
+        <div className="w-full md:w-1/2 flex flex-col justify-center items-center p-6 md:p-12 rounded-xl md:border bg-white border-gray-300">
+          <h2 className="text-2xl md:text-3xl font-bold mb-4">Welcome back!</h2>
+          <div className="text-center mb-6 md:mb-8 text-xs md:text-sm">
             <p className="text-gray-600">
               We're glad to see you again. Log in to pick up where you left off, access your personalized content, and explore what's new.
             </p>
@@ -37,16 +37,16 @@ const Signin = () => {
           <input
             type="text"
             placeholder="Username"
-            className="w-full border border-gray-300 rounded-3xl p-3 mb-4"
+            className="w-full border border-gray-300 rounded-3xl p-2 md:p-3 mb-3 md:mb-4 text-sm md:text-base"
           />
-          <div className="relative w-full mb-4">
+          <div className="relative w-full mb-3 md:mb-4">
             <input
               type={passwordVisible ? 'text' : 'password'}
               placeholder="Password"
-              className="w-full border border-gray-300 rounded-3xl p-3"
+              className="w-full border border-gray-300 rounded-3xl p-2 md:p-3 text-sm md:text-base"
             />
             <div
-              className="absolute right-3 top-4 cursor-pointer"
+              className="absolute right-3 top-3 md:top-4 cursor-pointer"
               onClick={togglePasswordVisibility}
             >
               {passwordVisible ? (
@@ -58,40 +58,40 @@ const Signin = () => {
           </div>
 
           {/* Forget password link */}
-          <div className="w-full text-right mb-6">
-            <a href="#" className="text-gray-500">Forget password?</a>
+          <div className="w-full text-right mb-4 md:mb-6">
+            <a href="#" className="text-gray-500 text-xs md:text-sm">Forget password?</a>
           </div>
 
           {/* Login Button */}
-          <div className="flex justify-center gap-4 mb-8 w-full">
-            <button className="bg-black text-white border px-32 border-gray-300 p-3 rounded-3xl w-full md:w-full">
+          <div className="flex justify-center gap-4 mb-6 md:mb-8 w-full">
+            <button className="bg-black text-white border px-20 md:px-32 border-gray-300 p-2 md:p-3 rounded-3xl w-full text-sm md:text-base">
               Login
             </button>
           </div>
 
           {/* Horizontal Line and "or continue with" */}
-          <div className="flex items-center w-full mb-4">
-            <div className="flex-grow border-t-2 border-gray-300"></div>
-            <span className="px-3 text-gray-500">or continue with</span>
+          <div className="flex items-center w-full mb-3 md:mb-4">
+            <div className="flex-grow border-t border-gray-300"></div>
+            <span className="px-2 md:px-3 text-gray-500 text-xs md:text-sm">or continue with</span>
             <div className="flex-grow border-t border-gray-300"></div>
           </div>
 
           {/* Social Buttons */}
-          <div className="flex gap-4">
-            <button className="p-3 bg-black rounded-full">
-              <img src={google} alt="Google" className="w-6 h-6" />
+          <div className="flex gap-3 md:gap-4">
+            <button className="p-2 md:p-3 bg-black rounded-full">
+              <img src={google} alt="Google" className="w-4 md:w-6 h-4 md:h-6" />
             </button>
-            <button className="p-3 bg-black rounded-full">
-              <img src={apple} alt="Apple" className="w-6 h-6" />
+            <button className="p-2 md:p-3 bg-black rounded-full">
+              <img src={apple} alt="Apple" className="w-4 md:w-6 h-4 md:h-6" />
             </button>
-            <button className="p-3 bg-black rounded-full">
-              <img src={facebook} alt="Facebook" className="w-6 h-6" />
+            <button className="p-2 md:p-3 bg-black rounded-full">
+              <img src={facebook} alt="Facebook" className="w-4 md:w-6 h-4 md:h-6" />
             </button>
           </div>
 
           {/* Not a member */}
-          <div className="mt-6">
-            <p className="text-center">
+          <div className="mt-4 md:mt-6">
+            <p className="text-center text-xs md:text-sm">
               <span className="text-gray-500">Not a member? </span>
               <a
                 href="#"
@@ -105,10 +105,11 @@ const Signin = () => {
         </div>
 
         {/* Right Section - Image */}
-        <div className="w-full bg-white md:bg-customgreen md:w-1/2 flex justify-center items-center flex flex-col p-8 md:p-12 rounded-xl">
-          <img src={signin} alt="signin" className="w-3/4 md:w-2/3 mb-6 rounded-lg" />
-          <p className="hidden md:block text-black text-lg text-center">
-          We’re excited to have you here again! Dive right back in to discover what’s new and pick up where you left off.            </p>
+        <div className="w-full md:bg-customgreen md:w-1/2 flex justify-center items-center flex-col p-6 md:p-12 rounded-xl">
+          <img src={signin} alt="signin" className="w-1/2 md:w-2/3 mb-4 md:mb-6 rounded-lg" />
+          <p className="hidden md:block text-black text-sm md:text-lg text-center">
+            We’re excited to have you here again! Dive right back in to discover what’s new and pick up where you left off.
+          </p>
         </div>
       </div>
     </div>
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -14,25 +14,25 @@ const Signup = () => {
   };
 
   return (
-    <div className="flex flex-col md:flex-row min-h-screen items-center justify-center p-4 md:p-16 md:px-20">
+    <div className="flex flex-col md:flex-row h-screen items-center justify-center p-4 md:p-16 md:px-20 bg-white">
       {/* Parent container with flex and equal height */}
       <div className="flex flex-col md:flex-row w-full h-full md:w-3/4 max-w-screen-xl">
         
         {/* Left Section - Show only photo for mobile */}
-        <div className="w-full md:w-1/2 flex justify-center items-center bg-white p-8 md:bg-customgreen md:p-12 rounded-xl">
+        <div className="w-full md:w-1/2 flex justify-center items-center p-4 md:p-12 md:bg-customgreen rounded-xl">
           <div className="flex flex-col items-center">
-            <img src={signup} alt="signup" className="w-3/4 md:w-2/3 mb-6 rounded-lg" />
+            <img src={signup} alt="signup" className="w-2/3 md:w-2/3 mb-4 md:mb-6 rounded-lg" />
             {/* Keep the <p> tag only for desktop view */}
-            <p className="hidden md:block text-black text-lg text-center">
+            <p className="hidden md:block text-black text-base md:text-lg text-center">
               Unlock exclusive features, personalize your experience, and stay connected with everything you love.
             </p>
           </div>
         </div>
 
         {/* Right Section - All content in column for mobile */}
-        <div className="w-full md:w-1/2 flex flex-col justify-center items-center p-8 md:p-12 rounded-xl border border-gray-300">
-          <h2 className="text-3xl font-bold mb-4">Create Account</h2>
-          <p className="text-gray-600 text-center mb-8">
+        <div className="w-full md:w-1/2 flex flex-col justify-center items-center p-6 md:p-12 bg-white rounded-xl md:border border-gray-300">
+          <h2 className="text-2xl md:text-3xl font-bold mb-3 md:mb-4">Create Account</h2>
+          <p className="text-gray-600 text-center text-sm md:text-base mb-6 md:mb-8">
             Join us and unlock a world of personalized experiences tailored just for you!
           </p>
 
@@ -40,56 +40,56 @@ const Signup = () => {
           <input
             type="text"
             placeholder="Name"
-            className="w-full border border-gray-300 rounded-3xl p-3 mb-4"
+            className="w-full border border-gray-300 rounded-3xl p-2 md:p-3 mb-3 md:mb-4 text-sm md:text-base"
           />
           <input
             type="email"
             placeholder="Email"
-            className="w-full border border-gray-300 rounded-3xl p-3 mb-4"
+            className="w-full border border-gray-300 rounded-3xl p-2 md:p-3 mb-3 md:mb-4 text-sm md:text-base"
           />
-          <div className="flex flex-col md:flex-row gap-4 mb-6 w-full">
+          <div className="flex flex-col md:flex-row gap-3 md:gap-4 mb-4 md:mb-6 w-full">
             <input
               type="password"
               placeholder="Password"
-              className="w-full border border-gray-300 rounded-3xl p-3"
+              className="w-full border border-gray-300 rounded-3xl p-2 md:p-3 text-sm md:text-base"
             />
             <input
               type="password"
               placeholder="Confirm Password"
-              className="w-full border border-gray-300 rounded-3xl p-3"
+              className="w-full border border-gray-300 rounded-3xl p-2 md:p-3 text-sm md:text-base"
             />
           </div>
 
           {/* Buttons */}
-          <div className="flex flex-col md:flex-row justify-center gap-4 mb-8 w-full">
-            <button className="bg-black text-white p-3 px-6 rounded-3xl w-full">
+          <div className="flex flex-col md:flex-row justify-center gap-3 md:gap-4 mb-6 md:mb-8 w-full">
+            <button className="bg-black text-white p-2 md:p-3 rounded-3xl w-full text-sm md:text-base">
               Register
             </button>
             <button
               onClick={handleSignInClick} // Navigate to SignIn page
-              className="bg-white text-black border px-6 border-gray-300 p-3 rounded-3xl w-full"
+              className="bg-white text-black border px-4 md:px-6 border-gray-300 p-2 md:p-3 rounded-3xl w-full text-sm md:text-base"
             >
               Sign In
             </button>
           </div>
 
           {/* Horizontal Line and "or continue with" */}
-          <div className="flex items-center w-full mb-4">
-            <div className="flex-grow border-t-2 border-gray-300"></div>
-            <span className="px-3 text-gray-500">or continue with</span>
+          <div className="flex items-center w-full mb-3 md:mb-4">
+            <div className="flex-grow border-t border-gray-300"></div>
+            <span className="px-2 md:px-3 text-gray-500 text-xs md:text-sm">or continue with</span>
             <div className="flex-grow border-t border-gray-300"></div>
           </div>
 
           {/* Social Buttons */}
-          <div className="flex gap-4">
-            <button className="p-3 bg-black rounded-full">
-              <img src={google} alt="Google" className="w-6 h-6" />
+          <div className="flex gap-3 md:gap-4">
+            <button className="p-2 md:p-3 bg-black rounded-full">
+              <img src={google} alt="Google" className="w-4 md:w-6 h-4 md:h-6" />
             </button>
-            <button className="p-3 bg-black rounded-full">
-              <img src={apple} alt="Apple" className="w-6 h-6" />
+            <button className="p-2 md:p-3 bg-black rounded-full">
+              <img src={apple} alt="Apple" className="w-4 md:w-6 h-4 md:h-6" />
             </button>
-            <button className="p-3 bg-black rounded-full">
-              <img src={facebook} alt="Facebook" className="w-6 h-6" />
+            <button className="p-2 md:p-3 bg-black rounded-full">
+              <img src={facebook} alt="Facebook" className="w-4 md:w-6 h-4 md:h-6" />
             </button>
           </div>
         </div>
